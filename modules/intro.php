490fa80195936be0fc0756eb6f371373951bfabb
Jon Masters <Jonathan_masters77@yahoo.co.uk>
2015-09-11 15:16:53 +0100

    Added Home page intro image transition plugin

--- modules/intro.php
+++ modules/intro.php
@@ -6,11 +6,8 @@
  */
 ?>
 <section class="tm_intro section js-height-full" id="section1">
-    <div class="hero-gif">
-        <!--  <img src="<?php //echo bloginfo('template_directory'); ?>/gui/video/home-hero.mp4" type="video/mp4"> -->
-    </div>
+   <div class="home-carousel"><?php echo do_shortcode('[image-carousel]'); ?></div>
     <div class="welcome">
         <h1 class="main-heading">Welcome to<br>Trinity Mirror<br>North West &amp; Wales</h1>
-        <h2 class="sub-heading">Trinity Mirror are one of the UK's largest multimedia publishers and regionally in the North West and Wales. We have a large media portfolio reaching and unduplicated audience of <strong>1.8 million adults</strong></h2>
     </div>
 </section>
