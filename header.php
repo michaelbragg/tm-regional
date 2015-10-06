035a276138d4c606bddaf24f4629cf1bb96e6a93
Jon Masters <Jonathan_masters77@yahoo.co.uk>
2015-10-06 14:20:04 +0100

    ie

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
  */
 
 ?><!DOCTYPE html>
-<!--[if lt IE 9]>    <html class="no-js gte-ie8" <?php language_attributes(); ?>> <![endif]-->
+<!--[if gt IE 7]>    <html class="no-js gt-ie7" <?php language_attributes(); ?>> <![endif]-->
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
