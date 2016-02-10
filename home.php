1a6b27c724a77fbd81c653adeafb735c487471a7
Michael Bragg <michael@michaelbragg.net>
2016-02-10 15:49:07 +0000

    Add layout and styling for 'Posts Page' template

---
+++ home.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * The 'Posts Page' template file.
+ *
+ * @package tm-regional
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<section class="kenburns hero-height hero hero--blog entry-header">
+		    <div class="container page-headline">
+		        <div class="hero-page-title">
+		             <?php get_the_title( '<p>', '</p>' ); ?>
+		        </div>
+		    </div>
+			</section><!-- .entry-header -->
+
+			<section class="container page-content">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+			<article>
+			<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+			<div class="post-link-bar">
+			<?php tm_regional_posted_on(); ?>
+			<?php tm_regional_entry_footer(); ?>
+			</div>
+			<?php the_excerpt(); ?>
+			<?php endwhile; ?>
+
+			<?php the_posts_navigation(); ?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+		<?php endif; ?>
+			</section>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
