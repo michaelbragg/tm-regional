5dc1acb454929ba1f424249149021ef16ec134af
Michael Bragg <michael@michaelbragg.net>
2016-02-24 10:39:48 +0000

    Improve visibility of select a brand text

--- archive-brands.php
+++ archive-brands.php
@@ -78,7 +78,7 @@ $my_posts = get_posts($args);
     <div class="tabs-arrow-down"></div>
       <div id='<?php echo $term->slug; ?>' class='tab-container <?php echo $term->slug; ?>-section-block' >
 
-        <h2><?php echo $term->name; ?><small class="brand-select-text"> / Select a brand</small></h2>
+        <h2><?php echo $term->name; ?><small class="tab-select-text"> <?php _e( 'Select a brand', 'tm-regional' ); ?></small></h2>
           <ul class='posts-menu tabs-menu etabs brand-select'>
             <?php foreach( $post_array as $post ): ?><li class='tab'><a href="#<?php echo $post->post_name; ?>"><?php the_title(); ?></a></li><?php endforeach;?>
           </ul>
